3fdf96e4376bcd47ea91f40bd951294963eae75b
Navid Nourani Vatani <navid@fleetspark.ai>
2024-12-05 12:22:44 +0100

    added data and script for HICP plots

---
+++ colors.py
@@ -0,0 +1,61 @@
+"""
+source https://www.simplifiedsciencepublishing.com/resources/best-color-palettes-for-scientific-figures-and-data-visualizations
+"""
+
+# Grayscale palette
+grayscale_colors = {
+    "G1": "#0d0d0d",
+    "G2": "#262626",
+    "G3": "#595959",
+    "G4": "#7f7f7f",
+    "G5": "#a1a1a1",
+    "G6": "#bababa",
+    "G7": "#d4d4d4",
+    "G8": "#ededed",
+}
+
+# Bright palette
+bright_colors = {
+    "Dark Blue": "#003a7d",
+    "Med Blue": "#008dff",
+    "Pink": "#ff73b6",
+    "Purple": "#c701ff",
+    "Green": "#4ecb8d",
+    "Orange": "#ff9d3a",
+    "Yellow": "#f9e858",
+    "Red": "#d83034",
+}
+
+# Muted palette
+muted_colors = {
+    "Gray": "#c8c8c8",
+    "Gold": "#f0c571",
+    "Teal": "#59a89c",
+    "Blue": "#0b81a2",
+    "Red": "#e25759",
+    "Dark Red": "#9d2c00",
+    "Purple": "#7e4794",
+    "Green": "#36b700",
+}
+
+# Alternating Light/Dark Pairs
+alternating_colors = {
+    "Light Blue": "#8fd7d7",
+    "Med Blue": "#00b0be",
+    "Light Pink": "#ff8ca1",
+    "Med Pink": "#f45f74",
+    "Light Green": "#bdd373",
+    "Med Green": "#98c127",
+    "Light Orange": "#ffcd8e",
+    "Med Orange": "#ffb255",
+}
+
+if __name__ == "__main__":
+    # Display the formatted palettes for reference
+    formatted_palettes = {
+        "Grayscale": grayscale_colors,
+        "Bright": bright_colors,
+        "Muted": muted_colors,
+        "Alternating": alternating_colors,
+    }
+    formatted_palettes
